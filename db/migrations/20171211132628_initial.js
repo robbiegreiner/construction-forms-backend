67e5ca89bdcd3ef92ee75928a5b44a250f6fb10e
Robbie Greiner <robbieg@gmail.com>
2017-12-12 14:35:37 -0700

    build up migration

--- db/migrations/20171211132628_initial.js
+++ db/migrations/20171211132628_initial.js
@@ -1,8 +1,35 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('employees', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('position');
+      table.string('email');
+      table.string('phone');
+    }),
+
+    knex.schema.createTable('projects', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('location');
+      table.boolean('union');
+      table.integer('lead_employee').unsigned();
+      table.foreign('lead_employee').references('employees.id');
+      table.boolean('public');
+    }),
+
+    knex.schema.createTable('employees_projects', (table) => {
+      table.integer('project_id').unsigned();
+      table.foreign('project_id').references('projects.id');
+      table.integer('employees_id').unsigned();
+      table.foreign('employees_id').references('employees.id');
+    })
+
+
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+
 };
