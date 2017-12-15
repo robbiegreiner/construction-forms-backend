dc3670bbb6e8eb8f80e6c296662f520933a65630
Alex Banister <alexbanister@gmail.com>
2017-12-15 12:49:50 -0700

    add seed for prod

--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,9 @@ module.exports = {
     migrations: {
       directory: './db/migrations'
     },
+    seeds: {
+      directory: './db/seeds/production'
+    },
     useNullAsDefault: true
   },
 
